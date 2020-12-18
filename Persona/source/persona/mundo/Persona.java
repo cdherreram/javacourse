85f805cceb69cb9ff09556c008d7a46d95a104f4
Cristian David Herrera Medina <cdherreram@unal.edu.co>
2020-12-17 20:15:46 -0500

    "Se adicionó el main al proyecto Persona y se adicionaron algunas funciones a la clase Persona - 17 Dic 2020"

--- Persona/source/persona/mundo/Persona.java
+++ Persona/source/persona/mundo/Persona.java
@@ -13,14 +13,15 @@ public class Persona {
 	private char genero; // el genero puede ser m y f --- 
 	private boolean esSoltero; // ser soltero es verdadero o falso
 	private String colorDePiel; //
-	Trabajo trabajoActual;
+	private Trabajo trabajoActual;
 	
 	//----------------------------------------------
 	//Constructor	
 	// El constructor necesita (eventualmente) parámetros
 	//----------------------------------------------
 	
-	public Persona (int pEdad, char pGenero, boolean pEsSoltero, String pColorDePiel, String pNombreOficio, String pNombreEmpresa, int pDuracion) {
+	public Persona (int pEdad, char pGenero, boolean pEsSoltero, String pColorDePiel,
+			String pNombreOficio, String pNombreEmpresa, int pDuracion) {
 		edad = pEdad;
 		genero = pGenero;
 		esSoltero = pEsSoltero;
@@ -48,11 +49,44 @@ public class Persona {
 		return colorDePiel;
 	}
 	
+	public Trabajo darTrabajoActual() {
+		return trabajoActual;
+	}
+	
 	public void cambiarEsSoltero(boolean nuevoEstado) {
 		esSoltero = nuevoEstado;
 	}
 	
 	public void imprimirNombreTrabajo() {
 		System.out.println(trabajoActual.darNombreOficioRealizado());
+	}
+	
+	public boolean esMayorDeEdad( ) {
+		if ( edad >= 18 )
+			return true;
+		else
+			return false;
+	}	
+	
+	//Aquí pongo el main
+	
+	public static void main(String[] args) {
+		//¿Cómo crear un objeto?
+		
+		//1. De qué clase es
+		//2. Cómo lo vamos a llamar (referencia)
+		//3. crear el objeto
+		
+		Persona persona1 = new Persona( 21, 'f', true, "Trigueño", "Contador", "IBM", 4);
+		Persona persona2 = new Persona( 40, 'f', false, "Albino", "Tejedor", "Lanas S.A.", 15);
+		
+		System.out.println(persona1.darColorDePiel());
+		System.out.println( "El trabajo actual de la persona 2 es " + 
+				persona2.darTrabajoActual().darNombreEmpresa() );
+		if ( persona1.esMayorDeEdad() == true) {
+			System.out.println("La persona 1 es mayor de edad");
+		} else {
+			System.out.println("La persona 1 NO  es mayor de edad");
+		}
 	}
 }
